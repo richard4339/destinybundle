90ac763a309679ffcd83461abc7f80ad5d1e69c3
Richard <richard@mozor.net>
2019-11-25 10:18:46 -0600

    WIP

--- src/DependencyInjection/DestinyClientExtension.php
+++ src/DependencyInjection/DestinyClientExtension.php
@@ -4,19 +4,23 @@
 namespace Destiny\ClientBundle\DependencyInjection;
 
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class DestinyClientExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
 
+        $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
 
-//        $definition = $container->getDefinition('destiny.client_id');
-//        $definition->replaceArgument(0, $config['twitter']['client_id']);
-//        $definition->replaceArgument(1, $config['twitter']['client_secret']);
+        $definition = $container->getDefinition('destiny_client.client');
+        $definition->setArgument(0, $config['client_id']);
+        $definition->setArgument(1, $config['client_secret']);
     }
 }
